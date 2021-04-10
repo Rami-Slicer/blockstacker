e3ca484e15cf55b73403888c439b53c2002d998a
Rami-Slicer <rami.slicer@gmail.com>
2021-04-10 14:18:43 -0500

    add test resources

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,37 @@
 #include <fstream>
 // #include <nbt.hpp>
 // #include <nbt++/io/stream_reader.h>
-#include <string>
+// #include <string>
 #include <minecraft-file.hpp>
 #include <memory>
+
+const char * PYTHONPATH = "../";
+
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
 // #include <nbtpp/nbt.hpp>
 
 int main(int, char**) {
+    setenv("PYTHONPATH", PYTHONPATH, 1);
+
+    PyObject *pName, *pModule, *pDict, *pFunc, *pValue, *presult;
+
     namespace mc = mcfile;
+    Py_Initialize();
+
+    PyObject* module = PyImport_ImportModule("mc2obj.mc2obj");
+    PyObject* convertFunc = PyObject_GetAttrString(module, "convert");
+
+    PyObject* convertResult = PyObject_CallFunction(convertFunc, "minecraft/", "minecraft:redstone_torch[lit=true]", "blockCache/");
+    
+    
 
     std::shared_ptr<mc::Region> region = mc::Region::MakeRegion("../r.0.0.mca");
     std::cout << region->chunkExists(1, 2) << std::endl;
     std::shared_ptr<mc::Chunk> chunk = region->chunkAt(1, 2);
 
-    // const mc::Block glowstone = new mc::Block("glowstone");
-    const std::shared_ptr<mc::Block const> p(new mc::Block("glowstone"));
+    
 
     
 
@@ -24,6 +41,8 @@ int main(int, char**) {
     // nbtpp::nbt n(f);
     
     // n.debug();
+
+    Py_Finalize();
     
     return 0;
 }
